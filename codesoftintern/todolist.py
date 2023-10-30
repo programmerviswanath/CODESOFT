72003fdfb0e7806f0e2f8560dcd25ebfb326e613
programmerviswanath <128244515+programmerviswanath@users.noreply.github.com>
2023-10-30 19:31:24 +0530

    Update todolist.py
    updated file

--- codesoftintern/todolist.py
+++ codesoftintern/todolist.py
@@ -1,46 +1,42 @@
-
-def add(x, y):
-    return x + y
-
-def subtract(x, y):
-    return x - y
-
-def multiply(x, y):
-    return x * y
-
-def divide(x, y):
-    if y == 0:
-        return "Error: Denominator should not be Zero"
-    return x / y
-
-while True:
-    print("Options:")
-    print("Enter 'a' for addition")
-    print("Enter 'b' for subtraction")
-    print("Enter 'c' for multiplication")
-    print("Enter 'd' for division")
-    print("Enter 'e' to end the program")
-    
-    choice = input("Please enter choice [a/b/c/d/e]: ")
-
-    if choice == 'e':
-        break
-
-    if choice in ("a", "b", "c", "d"):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-
-        if choice == "a":
-            print("Result: ", add(num1, num2))
-        elif choice == "b":
-            print("Result: ", subtract(num1, num2))
-        elif choice == "c":
-            print("Result: ", multiply(num1, num2))
-        elif choice == "d":
-            result = divide(num1, num2)
-            if isinstance(result, str):
-                print(result)
-            else:
-                print("Result: ", result)
+def display_tasks(tasks):
+    if not tasks:
+        print("No tasks in the to-do list.")
     else:
-        print("Invalid input")
+        print("\nTo-do List:")
+        for index, task in enumerate(tasks,start=1):
+            print(f"{index}. {task}")
+
+def add_task(tasks,new_tasks):
+    tasks.append(new_tasks)
+    print(f"Task '{new_tasks}' added to the to-do list.")
+
+def remove_task(tasks,task_index):
+    if 1 <= task_index <= len(tasks):
+        remove_task = tasks.pop(task_index - 1)
+        print(f"Task '{remove_task}' removed from the to-do list.")
+    else:
+        print("Invalid task index.")
+
+def main():
+    tasks = []
+
+    while True:
+        print("\nSelect an option:")
+        print("1. Display tasks\n2. Add tasks\n3. Remove tasks\n4. Quit")
+        choice = input("Enter your choice: ")
+        if choice == '1':
+            display_tasks(tasks)
+        elif choice == '2':
+            new_task = input("Enter the new task: ")
+            add_task(tasks,new_task)
+        elif choice == '3':
+            task_index = int(input("Enter the number to remove a task: "))
+            remove_task(tasks, task_index)
+        elif choice == '4':
+            print("Exiting the to-do list application.")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ =="__main__":
+    main()
